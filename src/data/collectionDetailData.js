45664cbabe82542655af251334d7855bd4350792
sssuearth <128416382+sssuearth@users.noreply.github.com>
2024-11-27 06:40:34 +0900

    일단 수정

--- src/data/collectionDetailData.js
+++ src/data/collectionDetailData.js
@@ -19,17 +19,17 @@ import west2 from '../assets/collection/detail/detail_tissue_2.png';
 const collectionDetailData = [
   {
     id: 1,
-    title: "Scarlett  Johans  son's  Snot Tissue",
-    subTitle: '스칼렛 요한슨이 코 푼 휴지',
-    image: tissue,
-    bestCost: '$ 955,555',
-    bidder: Ethan_Wang,
-    bidderName: 'Ethan_Wang',
+    title: 'LUNCH TICKET WITH WARREN BUFFET',
+    subTitle: '주식대가 워렌 버픽 과의 점심 식사권',
+    image: bupit,
+    bestCost: '$ 4,122,955,555',
+    bidder: Emily_Cooper,
+    bidderName: 'Emily_Cooper',
     defaultCost: '$5,300 (2008년)',
     changeCost: '$ 50,255 (+16년) 🔺',
     description:
-      "테스트1 테스트2 스칼렛 요한슨은 2008년 미국 인기 토크쇼인 '제이 레노의 투나잇 쇼'에 신작 영화 홍보차 출연해 자신이 코 푼 휴지를 자선단체에 기부했다. 스칼렛 요한슨은 당시 토크쇼에 출연해 자신이 사무엘 잭슨으로부터 감기가 옮았다며 '이 감기는 유명인들이 거친 감기니 값어치가 있지 않느냐'고 재치있게 말했다. 이에 토크쇼 진행자 제이 레노는 '당신이 2번 코 푼 휴지를 가난한 사람들을 위한 단체, ‘USA 하베스트’에 기부하는 것은 어떠냐'는 파격 제안을 했다. 스칼렛 요한슨은 이에 흔쾌히 응했고 그의 콧물, 립스틱 자국이 남은 휴지를 비닐에 담겨 사인과 함께 인터넷 경매사이트 이베이에 경매로 넘겼다.",
-    image2: tissue2,
+      "테스트1 테스트2 테스트3 스칼렛 요한슨은 2008년 미국 인기 토크쇼인 '제이 레노의 투나잇 쇼'에 신작 영화 홍보차 출연해 자신이 코 푼 휴지를 자선단체에 기부했다. 스칼렛 요한슨은 당시 토크쇼에 출연해 자신이 사무엘 잭슨으로부터 감기가 옮았다며 '이 감기는 유명인들이 거친 감기니 값어치가 있지 않느냐'고 재치있게 말했다. 이에 토크쇼 진행자 제이 레노는 '당신이 2번 코 푼 휴지를 가난한 사람들을 위한 단체, ‘USA 하베스트’에 기부하는 것은 어떠냐'는 파격 제안을 했다. 스칼렛 요한슨은 이에 흔쾌히 응했고 그의 콧물, 립스틱 자국이 남은 휴지를 비닐에 담겨 사인과 함께 인터넷 경매사이트 이베이에 경매로 넘겼다.",
+    image2: bupit2,
   },
   {
     id: 2,
@@ -88,6 +88,21 @@ const collectionDetailData = [
     image2: west2,
   },
   // 같은 방식으로 추가하세요.
+
+  {
+    id: 36,
+    title: "Scarlett Johansson's Snot Tissue",
+    subTitle: '스칼렛 요한슨이 코 푼 휴지',
+    image: west,
+    bestCost: '$ 955,555',
+    bidder: Sophia_Martinez,
+    bidderName: 'Sophia_Martinez',
+    defaultCost: '$5,300 (2008년)',
+    changeCost: '$ 150,255 (+16년) 🔺',
+    description:
+      "스칼렛 요한슨은 2008년 미국 인기 토크쇼인 '제이 레노의 투나잇 쇼'에 신작 영화 홍보차 출연해 자신이 코 푼 휴지를 자선단체에 기부했다. 스칼렛 요한슨은 당시 토크쇼에 출연해 자신이 사무엘 잭슨으로부터 감기가 옮았다며 '이 감기는 유명인들이 거친 감기니 값어치가 있지 않느냐'고 재치있게 말했다. 이에 토크쇼 진행자 제이 레노는 '당신이 2번 코 푼 휴지를 가난한 사람들을 위한 단체, ‘USA 하베스트’에 기부하는 것은 어떠냐'는 파격 제안을 했다. 스칼렛 요한슨은 이에 흔쾌히 응했고 그의 콧물, 립스틱 자국이 남은 휴지를 비닐에 담겨 사인과 함께 인터넷 경매사이트 이베이에 경매로 넘겼다.",
+    image2: west2,
+  },
 ];
 
 export default collectionDetailData;
