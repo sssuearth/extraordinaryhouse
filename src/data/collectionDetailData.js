0185cc2933b0fc51fa7e0d2697dfe6538cbe8068
sssuearth <128416382+sssuearth@users.noreply.github.com>
2024-12-01 21:46:03 +0900

    이미지 모두 완료

--- src/data/collectionDetailData.js
+++ src/data/collectionDetailData.js
@@ -6,14 +6,70 @@ import Sophia_Martinez from '../assets/collection/bidder/Sophia_Martinez.png';
 
 import tissue from '../assets/collection/detail/detail_tissue.png';
 import tissue2 from '../assets/collection/detail/detail_tissue_2.png';
-import bupit from '../assets/collection/detail/detail_bupit.png';
-import bupit2 from '../assets/collection/detail/detail_bupit_2.png';
 import remon from '../assets/collection/detail/detail_remon.png';
 import remon2 from '../assets/collection/detail/detail_remon_2.png';
-import dogbite from '../assets/collection/detail/detail_shoes.png';
-import dogbit2 from '../assets/collection/detail/detail_shoes_2.png';
 import west from '../assets/collection/detail/detail_west.png';
 import west2 from '../assets/collection/detail/detail_west2.png';
+import bupit from '../assets/collection/detail/detail_bupit.png';
+import bupit2 from '../assets/collection/detail/detail_bupit_2.png';
+import dogbite from '../assets/collection/detail/detail_shoes.png';
+import dogbit2 from '../assets/collection/detail/detail_shoes_2.png';
+import monlo from '../assets/collection/detail/detail_monlo.png';
+import monlo2 from '../assets/collection/detail/detail_monlo2.png';
+import nuget from '../assets/collection/detail/detail_nuget.png';
+import nuget2 from '../assets/collection/detail/detail_nuget2.png';
+import cadibi from '../assets/collection/detail/detail_cadib.png';
+import cadibi2 from '../assets/collection/detail/detail_cadib2.png';
+import game from '../assets/collection/detail/detail_game.png';
+import game2 from '../assets/collection/detail/detail_game2.png';
+import vote from '../assets/collection/detail/detail_vote.png';
+// import vote2 from '../assets/collection/detail/detail_vote2.png';
+import chitoce from '../assets/collection/detail/detail_chitoce.png';
+import chitoce2 from '../assets/collection/detail/detail_chitoce2.png';
+import maidenhood from '../assets/collection/detail/detail_maidenhood.png';
+import maidenhood2 from '../assets/collection/detail/detail_maidenhood2.png';
+import hat from '../assets/collection/detail/detail_hat.png';
+import hat2 from '../assets/collection/detail/detail_hat2.png';
+import roconrol from '../assets/collection/detail/detail_roconrol.png';
+import roconrol2 from '../assets/collection/detail/detail_roconrol2.png';
+import number from '../assets/collection/detail/detail_number.png';
+//import number2 from '../assets/collection/detail/detail_number2.png';
+import meteorite from '../assets/collection/detail/detail_meteorit.png';
+import meteorite2 from '../assets/collection/detail/detail_meteorit2.png';
+import plake from '../assets/collection/detail/detail_plake.png';
+import plake2 from '../assets/collection/detail/detail_plake2.png';
+import ian from '../assets/collection/detail/detail_ian.png';
+import ian2 from '../assets/collection/detail/detail_ian2.png';
+import gum from '../assets/collection/detail/detail_gum.png';
+import gum2 from '../assets/collection/detail/detail_gum2.png';
+import ipad from '../assets/collection/detail/detail_ipad.png';
+import ipad2 from '../assets/collection/detail/detail_ipad2.png';
+import sandwitch from '../assets/collection/detail/detail_sandwitch.png';
+import sandwitch2 from '../assets/collection/detail/detail_sandwitch2.png';
+import stones from '../assets/collection/detail/detail_stones.png';
+//import stones2 from '../assets/collection/detail/detail_stones2.png';
+import fishman from '../assets/collection/detail/detail_fishman.png';
+import fishman2 from '../assets/collection/detail/detail_fishman2.png';
+import cake from '../assets/collection/detail/detail_cake.png';
+import cake2 from '../assets/collection/detail/detail_cake2.png';
+import mud from '../assets/collection/detail/detail_mud.png';
+import mud2 from '../assets/collection/detail/detail_mud2.png';
+import toilet from '../assets/collection/detail/detail_toilet.png';
+import toilet2 from '../assets/collection/detail/detail_toilet2.png';
+import richdog from '../assets/collection/detail/detail_richdog.png';
+import richdog2 from '../assets/collection/detail/detail_richdog2.png';
+import hair from '../assets/collection/detail/detail_hair.png';
+import hair2 from '../assets/collection/detail/detail_hair2.png';
+import pants from '../assets/collection/detail/detail_pants.png';
+import pants2 from '../assets/collection/detail/detail_pants2.png';
+import underwear from '../assets/collection/detail/detail_underwear.png';
+import underwear2 from '../assets/collection/detail/detail_underwear2.png';
+import domain from '../assets/collection/detail/detail_domain.png';
+//import domain2 from '../assets/collection/detail/detail_domain2.png';
+import doritos from '../assets/collection/detail/detail_doritos.png';
+import doritos2 from '../assets/collection/detail/detail_doritos2.png';
+import test from '../assets/collection/detail/detail_test.png';
+import test2 from '../assets/collection/detail/detail_test2.png';
 
 // data.js
 const collectionDetailData = [
@@ -74,7 +130,7 @@ const collectionDetailData = [
     image2: bupit2,
   },
   {
-    id: 6,
+    id: 5,
     title: "Scarlett Johansson's Snot Tissue",
     subTitle: '스칼렛 요한슨이 코 푼 휴지',
     image: dogbite,
@@ -87,12 +143,11 @@ const collectionDetailData = [
       "스칼렛 요한슨은 2008년 미국 인기 토크쇼인 '제이 레노의 투나잇 쇼'에 신작 영화 홍보차 출연해 자신이 코 푼 휴지를 자선단체에 기부했다. 스칼렛 요한슨은 당시 토크쇼에 출연해 자신이 사무엘 잭슨으로부터 감기가 옮았다며 '이 감기는 유명인들이 거친 감기니 값어치가 있지 않느냐'고 재치있게 말했다. 이에 토크쇼 진행자 제이 레노는 '당신이 2번 코 푼 휴지를 가난한 사람들을 위한 단체, ‘USA 하베스트’에 기부하는 것은 어떠냐'는 파격 제안을 했다. 스칼렛 요한슨은 이에 흔쾌히 응했고 그의 콧물, 립스틱 자국이 남은 휴지를 비닐에 담겨 사인과 함께 인터넷 경매사이트 이베이에 경매로 넘겼다.",
     image2: dogbit2,
   },
-  // 같은 방식으로 추가
   {
-    id: 7,
+    id: 6,
     title: "Scarlett Johansson's Snot Tissue",
     subTitle: '스칼렛 요한슨이 코 푼 휴지',
-    image: dogbite,
+    image: monlo,
     bestCost: '$ 955,555',
     bidder: Sophia_Martinez,
     bidderName: 'Sophia_Martinez',
@@ -100,7 +155,22 @@ const collectionDetailData = [
     changeCost: '$ 150,255 (+16년) 🔺',
     description:
       "스칼렛 요한슨은 2008년 미국 인기 토크쇼인 '제이 레노의 투나잇 쇼'에 신작 영화 홍보차 출연해 자신이 코 푼 휴지를 자선단체에 기부했다. 스칼렛 요한슨은 당시 토크쇼에 출연해 자신이 사무엘 잭슨으로부터 감기가 옮았다며 '이 감기는 유명인들이 거친 감기니 값어치가 있지 않느냐'고 재치있게 말했다. 이에 토크쇼 진행자 제이 레노는 '당신이 2번 코 푼 휴지를 가난한 사람들을 위한 단체, ‘USA 하베스트’에 기부하는 것은 어떠냐'는 파격 제안을 했다. 스칼렛 요한슨은 이에 흔쾌히 응했고 그의 콧물, 립스틱 자국이 남은 휴지를 비닐에 담겨 사인과 함께 인터넷 경매사이트 이베이에 경매로 넘겼다.",
-    image2: dogbit2,
+    image2: monlo2,
+  },
+  // 같은 방식으로 추가
+  {
+    id: 7,
+    title: "Scarlett Johansson's Snot Tissue",
+    subTitle: '스칼렛 요한슨이 코 푼 휴지',
+    image: vote,
+    bestCost: '$ 955,555',
+    bidder: Sophia_Martinez,
+    bidderName: 'Sophia_Martinez',
+    defaultCost: '$5,300 (2008년)',
+    changeCost: '$ 150,255 (+16년) 🔺',
+    description:
+      "스칼렛 요한슨은 2008년 미국 인기 토크쇼인 '제이 레노의 투나잇 쇼'에 신작 영화 홍보차 출연해 자신이 코 푼 휴지를 자선단체에 기부했다. 스칼렛 요한슨은 당시 토크쇼에 출연해 자신이 사무엘 잭슨으로부터 감기가 옮았다며 '이 감기는 유명인들이 거친 감기니 값어치가 있지 않느냐'고 재치있게 말했다. 이에 토크쇼 진행자 제이 레노는 '당신이 2번 코 푼 휴지를 가난한 사람들을 위한 단체, ‘USA 하베스트’에 기부하는 것은 어떠냐'는 파격 제안을 했다. 스칼렛 요한슨은 이에 흔쾌히 응했고 그의 콧물, 립스틱 자국이 남은 휴지를 비닐에 담겨 사인과 함께 인터넷 경매사이트 이베이에 경매로 넘겼다.",
+    // image2: vote2,
   },
   {
     id: 8,
